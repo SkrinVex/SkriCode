5e5d37eca429bcb9268b5ec69cfe26fd498ca5de
komari <foundationscp11@gmail.com>
2026-04-28 14:42:59 +0500

    update 1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "su.SkrinVex.SkriPts"
         minSdk = 24
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.3"
+        versionCode = 15
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
